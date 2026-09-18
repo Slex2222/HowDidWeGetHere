81195bc8795275754820b4fe062714cb2c74a21e
Slex2222 <11532@student.rodenborch.nl>
2026-09-19 00:29:30 +0200

    Added components

---
+++ Globals/Components/GlobalComponents/MovementComponent.gd
@@ -0,0 +1,14 @@
+class_name VelocityComponent
+extends Node2D
+
+@export_category("Movement Specifics")
+@export var Speed: float
+
+@export_category("Object Imports")
+@export var SelectedMoveableObject: CharacterBody2D
+
+func Movement(ObjectSpeed: float, Direction: Vector2) -> void:
+	SelectedMoveableObject.velocity = ObjectSpeed * Direction
+
+func _process(_delta: float) -> void:
+	SelectedMoveableObject.move_and_slide()
